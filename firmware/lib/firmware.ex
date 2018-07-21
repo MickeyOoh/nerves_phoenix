bd08cddbc8360f20be031c55b9670bdb334a79ed
MickeyOoh <mikio.westvil@gmail.com>
2018-07-21 09:58:45 -0400

    first commit

---
+++ firmware/lib/firmware.ex
@@ -0,0 +1,28 @@
+defmodule Firmware do
+  use Application
+
+  #@interface Application.get_env(:firmware, :interface, :eth0)
+  @interface Application.get_env(:firmware, :interface, :wlan0)
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+    
+    # Define workers and child supervisors to be supervised
+    children = [
+      supervisor(Phoenix.PubSub.PG2, [Nerves.PubSub, [poolsize: 1]]),
+      worker(Task, [fn -> start_network() end], restart: :transient)
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Fw.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def start_network do
+    IO.puts to_string(@interface)
+    Nerves.Network.setup(to_string(@interface))
+  end
+end
